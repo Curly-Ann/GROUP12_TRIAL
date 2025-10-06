16fe4a37b0a9e3df1611aed24048ef4f18fd5312
lougenlou <gbandin@gbox.adnu.edu.ph>
2025-10-07 01:39:25 +0800

    USR-03

--- backend_struct/api/models.py
+++ backend_struct/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=255)
+    role = models.CharField(max_length=50, default='user')
+    
+    def __str__(self):
+        return self.username
 # Create your models here.
