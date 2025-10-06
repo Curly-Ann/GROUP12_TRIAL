09a8258aedf784d2b16ea0e3903b03f1d29dff44
unknown <lhufancia@gbox.adnu.edu.ph>
2025-10-07 00:34:21 +0800

    [FE1-01] Setup frontend project structure.

--- backend_struct/backend_struct/urls.py
+++ backend_struct/backend_struct/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'base.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
